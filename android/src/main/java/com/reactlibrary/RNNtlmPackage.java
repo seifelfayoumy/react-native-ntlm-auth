e4a6b781b220bd5afcfc23cfbb9d6b83b466db6a
Andrei Radoi <andrei.radoi@neurony.ro>
2020-04-28 17:53:14 +0300

    add android files rn < 0.60

--- android/src/main/java/com/reactlibrary/RNNtlmPackage.java
+++ android/src/main/java/com/reactlibrary/RNNtlmPackage.java
@@ -1,3 +1,4 @@
+
 package com.reactlibrary;
 
 import java.util.Arrays;
@@ -9,15 +10,19 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
-
-public class NtlmAuthPackage implements ReactPackage {
+public class RNNtlmPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Arrays.<NativeModule>asList(new NtlmAuthModule(reactContext));
+      return Arrays.<NativeModule>asList(new RNNtlmModule(reactContext));
+    }
+
+    // Deprecated from RN 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+      return Collections.emptyList();
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Collections.emptyList();
+      return Collections.emptyList();
     }
 }
